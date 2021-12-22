c1c428b7fa1b8eb109f6e128195d7ca3c1734e8a
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-12-21 23:52:15 -0400

    chore: remove string.split as hilbish provides it

--- hilbifetch.lua
+++ hilbifetch.lua
@@ -17,24 +17,6 @@ ascii =
 ⠀⠀⠀⠀⠘⢿⣷⣽⣿⡿⡎⠛⠋⠁⠀⠀⠀⠀⢳⢿⣽⣷⣿⣝⢿⡿⠀
 ⠀⠀⠀⠀⠀⠀⠉⠙⠻⠋⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣽⣾⡿⠟⠁⠀]]
 
--- Utility Functions
-function string.split(str, delimiter)
-	local result = {}
-	local from = 1
-	
-	local delim_from, delim_to = string.find(str, delimiter, from)
-	
-	while delim_from do
-		table.insert(result, string.sub(str, from, delim_from - 1))
-		from = delim_to + 1
-		delim_from, delim_to = string.find(str, delimiter, from)
-	end
-	
-	table.insert(result, string.sub(str, from))
-
-	return result
-end
-
 function longest(arr)
 	local longestint = 0
 	local longestidx = 1
