93eac213475b7bb4e7d657c25faef1f4c46d5e11
Joshument <74075169+Joshument@users.noreply.github.com>
2022-05-06 09:14:33 -0400

    feat: added memory section

--- hilbifetch.lua
+++ hilbifetch.lua
@@ -5,7 +5,7 @@ local infoidx = {}
 
 -- ASCII art
 ascii =
-[[  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣐⣶⣶⣀⡀⣤⠄⠀⠀⠀⠀⠀⠀⠀⠀
+[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣐⣶⣶⣀⡀⣤⠄⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⣠⣴⣖⢶⣶⣦⣀⢰⣿⣿⣿⣿⠏⠀⠀⣐⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⣸⣿⣿⣿⣯⣻⣿⣿⢹⣿⣿⣿⡟⠠⠨⠪⠂⣰⣦⣄⠀⠀⠀
 ⠀⠀⠀⢠⢯⣶⣮⢿⣿⣷⡽⣿⡘⣿⡿⣿⠁⡑⠡⠃⣰⣿⣿⣿⣦⠀⠀
@@ -91,6 +91,25 @@ function info(tbl)
 				f:close()
 
 				return sh:gsub('\n', '')
+			elseif k == 'memory' then
+				local f = io.open('/proc/meminfo', 'rb')
+				local meminfo = f:read '*a'
+				local memTotal, usedMem
+				if meminfo then
+					memTotal = meminfo:sub(select(1, meminfo:find 'MemTotal:' ), -1)
+					memTotal = memTotal:sub(1, select(2, memTotal:find '\n' ))
+						:gsub('MemTotal:%s+', '')
+						:gsub(' kB\n', '')
+			
+					usedMem = io.popen("free | grep Mem"):read("*a"):gsub("[%a%p]+%s+%d+%s+", '')
+					local i, j = usedMem:find("%d+")
+					usedMem = usedMem:sub(i, j)
+				else
+					memTotal = 0
+					usedMem = 0
+				end
+			
+				return string.format('%.0f/%.0fMiB', tonumber(usedMem) / 1024, tonumber(memTotal) / 1024)
 			end
 		end
 		infotable[k] = {name = v, infofunc = _infofunc}
